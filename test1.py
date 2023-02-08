56207a4c0974c1de8dd03f0e2929aea04fe4eb81
sanjulitrivedi <124141736+sanjulitrivedi@users.noreply.github.com>
2023-02-08 14:24:48 +0530

    Delete test1.py

--- test1.py
+++
@@ -1,9 +0,0 @@
-import boto3  # pip install boto3
-
-# Let's use Amazon S3
-s3 = boto3.client("s3")
-s3.upload_file(
-    Filename="/Users/sanjulitrivedi/Documents/orm_Session/test1.py",
-    Bucket="test1234567891234567",
-    Key="uploaded_by_Sanjuli",
-)
